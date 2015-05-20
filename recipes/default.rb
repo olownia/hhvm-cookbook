b79d68b9e86bb869615c93b2846f2746e12cada5
Mariusz Ołownia <ollownia@gmail.com>
2015-05-20 22:09:00 +0200

    hhvm service

--- recipes/default.rb
+++ recipes/default.rb
@@ -8,3 +8,8 @@ case node['hhvm']['installation_type']
   else
     raise 'Invalid installation type: '.concat(node['hhvm']['installation_type'])
 end
+
+service 'hhvm' do
+  supports status: true, restart: true, reload: true
+  action   :start
+end
